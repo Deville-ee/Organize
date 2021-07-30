4bc72377625bb3bfb8c8c306f16480430a6cfd2f
Wouter Joris <wouter.joris@gmail.com>
2021-07-30 15:56:50 +0200

    Add Extra folder install option. Add Extra extensions to the array Fix Typo in About section
    organize.sh : Change all $HOME Variables to ${HOME} to maintain compatibility.
    organize.sh : Add find symlink option (In case your home folder sit's on an 2nd HD)
    organize.sh : Version 3: Add config file system and root execution check
    .gitignore: Ignore all *.config files

--- organize.sh
+++ organize.sh
@@ -6,57 +6,90 @@
 # Creation date : 28-07-2021
 # Edit date : 30-07-2021
 
-version="2.1"
+OLD_IFS=$IFS
+IFS="
+"
 
+version="3"
+
+# Version 3   # Add an external config file which will be used as a guide line to organize your folder system. When no config file exists a new one will be created 
+#             # With the current file system as a guideline (Handy in order to backup your folder structure). 
+#             # Add extra root execution check
+# Version 2.3 # Change all ${HOME} Variables to ${HOME} to maintain compatibility.
+# Version 2.2 # Add Extra extensions like: MTS APK WEBP
 # Version 2.1 # Add extra function for the creation of your personal folders. Add Your folders to the "extra_folders" array
 # Version 2   # Forked the original W3NDO script https://github.com/W3NDO/Organize and add a routine for the file organisation. In this way only folders which are needed are created. 
 
-# Todo : Determine a system to conquer the perpetual choice if the files are rubbish or not. The idea of distributing these into specialized folder's root can make a good start.
-#        This will keep in fact your Downloads folder clean and forces you to maintain a folder structure in the other parts of your $HOME folder.
+# Todo  : Determine a system to conquer the perpetual choice if the files are rubbish or not. The idea of distributing these into specialized folder's root can make a good start.
+#         This will keep in fact your Downloads folder clean and forces you to maintain a folder structure in the other parts of your ${HOME} folder.
 #
-# Idea :  Remove the folders which became empty? Maybe keep a list of previous folder structure in order to re-install a previous folder structure.
-# 
+# Ideas :  + Remove the folders which became empty? Maybe keep a list of previous folder structure in order to re-install a previous folder structure.
+#
+#          + Scan the folder structure and add the amount of files to the folder's name
+#          + Add a rule system
+#          + Add file overwrite detection
 
 
-About="This script organizes a specific target folder by file types and moves these into a designated folder"
+
+About="This script organizes a specific target folder by file types and moves the into a designated folder"
 
 #Specify the target folder
-target_folder="$HOME/Downloads"
+target_folder="${HOME}/Downloads"
 
-audio_f="$HOME/Music"
-video_f="$HOME/Videos"
-pdf_f="$HOME/Documents/Office/PDF_Files"
-docs_f="$HOME/Documents/Office/Word_Docs"
-power_f="$HOME/Documents/Office/Powerpoints"
-scripts_f="$HOME/Documents/Script_Collection"
-image_f="$HOME/Pictures"
-note_f="$HOME/Documents/Office/Notebooks_Files"
-pack_f="$HOME/Documents/Archives/Debians"
-archive_f="$HOME/Documents/Archives/Tarballs"
-rar_f="$HOME/Documents/Archives/Compresses_Files"
+#Specify the config file
+config_file="folder_structure.config"
 
-extra_folders=("$HOME/bin" "$HOME/Virtual_Machines" "$HOME/Documents/Software/Linux" "$HOME/Documents/Personal/" "$HOME/Documents/Office/Text_Files" "$HOME/Documents/Secure" "$HOME/Workspace" "$HOME/Documents/Work" "$HOME/Pictures/Work" )
 
-folder_array=( "${audio_f}" "${video_f}" "${pdf_f}" "${docs_f}" "${power_f}" "${script_f}" "${image_f}" "${spread_f}" "${note_f}" "${debian_f}" "${compr_f}" "${txz_f}")
+
+extra_folders=("${HOME}/Pictures/" )
 
 audio_array=( "*.mp3" "*.m4a" "*.flac" "*.aac" "*.ogg" "*.wav" )
-video_array=("*.mp4" "*.mov" "*.avi" "*.mpg" "*.mpeg" "*.webm" "*.mpv" "*.mp2" "*.wmv" )
+video_array=("*.mp4" "*.mov" "*.avi" "*.mpg" "*.mpeg" "*.webm" "*.mpv" "*.mp2" "*.wmv" "*.MTS" )
 pdf_array=("*.pdf")
-docs_array=("*.doc" "*.docx" "*.txt" "*.odt" "*.xls")
-power_f=("*.ppt" "*.pptx")
+docs_array=("*.doc" "*.docx" "*.odt" "*.xls")
+text_array=("*.txt")
+power_array=("*.ppt" "*.pptx")
 scripts_array=("*.py" "*.rb" "*.sh")
-image_array=( "*.png" "*.jpg" "*.jpeg" "*.tif" "*.tiff" "*.bpm" "*.gif" "*.eps" "*.raw" )
+image_array=( "*.png" "*.jpg" "*.jpeg" "*.tif" "*.tiff" "*.bpm" "*.gif" "*.eps" "*.raw" "*.webp" )
 note_array=("*.ipynb")
-pack_array=("*.deb" "*.pkg.tar.xz" )
+pack_array=("*.deb" "*.pkg.tar.xz" ".apk" )
 archive_array=("*.tar.*" "*.xz" "*.txz" "*.tgz" "*.7z")
 rar_array=("*.zip" "*.rar" "*.iso" "*.img" )
 
+function create_config_file()
+{
+    echo "# This config file will be used as a guideline by organize.sh"  > ${config_file}
+    echo -e "# Add folders here under 'extra folders' in order to create automatically your disired folder structure\n\n" >> ${config_file}
+    echo "audio_f="$\{HOME\}/Music"" >> ${config_file}
+    echo "video_f="$\{HOME\}/Videos"" >> ${config_file}
+    echo "pdf_f="$\{HOME\}/Documents/Office/PDF_Files"" >> ${config_file}
+    echo "docs_f="$\{HOME\}/Documents/Office/Word_Docs"" >> ${config_file}
+    echo "txt_f="$\{HOME\}/Documents/Office/Text_Files"" >> ${config_file}
+    echo "power_f="$\{HOME\}/Documents/Office/Powerpoints"" >> ${config_file}
+    echo "scripts_f="$\{HOME\}/Documents/Script_Collection"" >> ${config_file}
+    echo "image_f="$\{HOME\}/Pictures"" >> ${config_file}
+    echo "note_f="$\{HOME\}/Documents/Office/Notebooks_Files"" >> ${config_file}
+    echo "pack_f="$\{HOME\}/Documents/Archives/Debians"" >> ${config_file}
+    echo "archive_f="$\{HOME\}/Documents/Archives/Tarballs"" >> ${config_file}
+    echo -e "rar_f="$\{HOME\}/Documents/Archives/Compresses_Files"\n\n" >> ${config_file}
+    echo "extra_folders=(" >> ${config_file}
 
+    for path in $(find ~/  -maxdepth 2  -not -path '*/\.*' -type d  -iname "[a-z]*"  | uniq)
+    do
+        echo "${path}" | sed "s#${HOME}#$\{HOME\}#"  >> ${config_file}
+    done
+
+    echo -e  ")\n\n" >> ${config_file}
+    echo "folder_array=( "$\{audio_f\}" "$\{video_f\}" "$\{pdf_f\}" "$\{docs_f\}" "$\{power_f\}" "$\{script_f\}" "$\{image_f\}" "$\{spread_f\}" "$\{note_f\}" "$\{debian_f\}" "$\{compr_f\}" "$\{txz_f\}")"  >>  ${config_file}
+
+
+}
 
 function create_folder()
 {
     if [[ ! -d "${1}"  ]]
     then
+        echo "INFO: Creating folder: ${1}"
         mkdir -p "${1}"
     fi
 }
@@ -66,6 +99,7 @@ export -f create_folder
 function move_file()
 {
     create_folder "${2}"
+    echo "INFO: Moving file : ${1}  to ${2}"
     mv "${1}" "${2}"
 }
 
@@ -78,7 +112,7 @@ function sorting()
 
     for (( i=1; i<${arr_len} ; i++ ))
     do
-       find "${target_folder}" -maxdepth 1 -path "${target_folder}/${1}" -prune -false -o -iname "${arr[$i]}"  -exec /bin/bash -c "move_file '{}' \"${target_folder}/${1}\"" \;
+       find -L "${target_folder}" -maxdepth 1 -path "${target_folder}/${1}" -prune -false -o -iname "${arr[$i]}"  -exec /bin/bash -c "move_file '{}' ${1}" \;
     done
 }
 
@@ -97,16 +131,31 @@ function create_extra_folders()
     done
 }
 
+if [[ $(id -u) -eq 0 ]]
+then
+    echo "ERROR: You are the super user, execute the script as the normal user ($USER)"
+    exit 1
+fi
+
+if [[ ! -e ${config_file} ]]
+then
+    echo "WARNING: ${config_file} not found on this system, creating a new template based on this system's folder structure"
+    create_config_file
+    exit 1
+else
+    source ${config_file}
+fi
+
 create_extra_folders  "${extra_folders[@]}"
 
 echo "Organizing your messy ${target_folder} folder"
 
-#Audio Files
 sorting "${audio_f}"    "${audio_array[@]}"
 sorting "${video_f}"    "${video_array[@]}"
 sorting "${pdf_f}"      "${pdf_array[@]}"
 sorting "${power_f}"    "${power_array[@]}"
 sorting "${docs_f}"     "${docs_array[@]}"
+sorting "${txt_f}"     "${text_array[@]}"
 sorting "${scripts_f}"  "${scripts_array[@]}"
 sorting "${image_f}"    "${image_array[@]}"
 sorting "${note_f}"     "${note_array[@]}"
@@ -115,4 +164,6 @@ sorting "${archive_f}"  "${archive_array[@]}"
 sorting "${rar_f}"      "${rar_array[@]}"
 
 echo "All sorted!!"
+
+IFS=$OLD_IFS
 
